4f9896df81bf97a831bd418b03ac5a02b0119e90
171250640 <171250640@smail.nju.edu.cn>
2020-03-07 15:17:54 +0800

    添加获取顶点集合方法

--- src/main/java/com/old2dimension/OCEANIA/vo/DomainVO.java
+++ src/main/java/com/old2dimension/OCEANIA/vo/DomainVO.java
@@ -4,6 +4,8 @@ import com.old2dimension.OCEANIA.po.Domain;
 import com.old2dimension.OCEANIA.po.Edge;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DomainVO {
     private int verticesNum;
@@ -45,5 +47,20 @@ public class DomainVO {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public ArrayList<FuncInfoForm> getVertices(){
+        ArrayList<FuncInfoForm> vertices=new ArrayList<FuncInfoForm>();
+        for(int i=0; i<edgeVOS.size(); i++){
+            EdgeVO curEdge=edgeVOS.get(i);
+            if(!vertices.contains(curEdge.getStart())){
+                vertices.add(curEdge.getStart());
+            }
+            if(!vertices.contains(curEdge.getEnd())){
+                vertices.add(curEdge.getEnd());
+            }
+        }
+        return vertices;
+
     }
 }
