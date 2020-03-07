6d239abffdd64044e3e617860e55a9a12af780ff
171250640 <171250640@smail.nju.edu.cn>
2020-03-07 15:33:40 +0800

    修改输出错误

--- src/main/java/com/old2dimension/OCEANIA/OceaniaRunner.java
+++ src/main/java/com/old2dimension/OCEANIA/OceaniaRunner.java
@@ -19,16 +19,7 @@ public class OceaniaRunner implements ApplicationRunner {
     GraphCalculateImpl graphCalculate;
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        //-------初始化最初的紧密度筛选为0----------
-        WeightForm initialWeight=new WeightForm();
-        initialWeight.setWeightName("closeness");
-        initialWeight.setWeightValue(0);
-        ArrayList<WeightForm> initialWeights=new ArrayList<WeightForm>();
-        initialWeights.add(initialWeight);
-
-        //------初始化图数据-------------
-        graphCalculate.initializeGraph("call_dependencies_update.txt");
-        graphCalculate.getConnectedDomains(initialWeights);
+        initializeGraph(graphCalculate,"call_dependencies_update.txt");
 
         //------打印顶点、边和连通域数目-----
         printGraphInfo(graphCalculate);
@@ -37,7 +28,19 @@ public class OceaniaRunner implements ApplicationRunner {
 
     }
 
-    private void printGraphInfo(GraphCalculateImpl graphCalculate) throws Exception{
+    public void initializeGraph(GraphCalculateImpl graphCalculate,String filename){
+        //-------初始化最初的紧密度筛选为0----------
+        WeightForm initialWeight=new WeightForm();
+        initialWeight.setWeightName("closeness");
+        initialWeight.setWeightValue(0);
+        ArrayList<WeightForm> initialWeights=new ArrayList<WeightForm>();
+        initialWeights.add(initialWeight);
+
+        //------初始化图数据-------------
+        graphCalculate.initializeGraph(filename);
+        graphCalculate.getConnectedDomains(initialWeights);
+    }
+    public void printGraphInfo(GraphCalculateImpl graphCalculate) throws Exception{
         try {
             System.out.println("图中顶点数为：" + (graphCalculate.allVertexes.size() + ""));
             System.out.println("图中边数目为：" + (graphCalculate.allEdges.size() + ""));
@@ -48,16 +51,17 @@ public class OceaniaRunner implements ApplicationRunner {
         }
     }
 
-    private void closenessFilter(GraphCalculateImpl graphCalculate) throws NumberFormatException{
+    public void closenessFilter(GraphCalculateImpl graphCalculate) throws NumberFormatException{
         try {
             Scanner sc = new Scanner(System.in);
 
             double closeness=-1.0;
-            while (closeness<0){
+            while (closeness<0||closeness>1){
                 System.out.print("请输入用于过滤的紧密度阈值(非负小数或整数)：");
                 String closenessStr = sc.nextLine();
                 closeness = Double.parseDouble(closenessStr);
-                if(closeness<0)System.out.println("紧密度阈值必须大于等于零！");
+                if(closeness<0)System.out.println("紧密度阈值必须大于等于0！");
+                if(closeness>1)System.out.println("紧密度阈值必须小于等于1！");
             }
 
             WeightForm weightForm=new WeightForm();
