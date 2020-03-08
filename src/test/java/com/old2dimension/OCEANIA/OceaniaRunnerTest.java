9b28429b34c1b7eddf92644354797ea474be9b16
171250640 <171250640@smail.nju.edu.cn>
2020-03-08 17:10:12 +0800

    修改测试，换行符去除

--- src/test/java/com/old2dimension/OCEANIA/OceaniaRunnerTest.java
+++ src/test/java/com/old2dimension/OCEANIA/OceaniaRunnerTest.java
@@ -37,29 +37,29 @@ public class OceaniaRunnerTest {
 
         oceaniaRunner.initializeGraph(graphCalculate,"src/test/resources/runner_test_dependency_data.txt");
         oceaniaRunner.closenessFilter(graphCalculate);
-        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------\r\n" +
-                "连通域顶点数：3\r\n" +
-                "Node1: Class1:Func5()\r\n" +
-                "Node2: Class1:Func7()\r\n" +
-                "Node3: Class1:Func6(java.lang.long)\r\n\r\n"+
-                "Edge1: Class1:Func5() -- 0.6666 --> Class1:Func7()\r\n"+
-                "Edge2: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)\r\n"+
-                "-------连通域2-------\r\n" +
-                "连通域顶点数：2\r\n" +
-                "Node1: Class1:Func1()\r\n" +
-                "Node2: Class1:Func2()\r\n\r\n" +
-                "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()\r\n"+
-                        "-------连通域3-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func8()\r\n" +
-                        "Node2: Class1:Func9()\r\n\r\n" +
-                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()\r\n"+
-                        "-------连通域4-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func11()\r\n" +
-                        "Node2: Class1:Func14()\r\n\r\n" +
-                        "Edge1: Class1:Func11() -- 1.0000 --> Class1:Func14()\r\n".replaceAll("[\\t\\n\\r]", "")
-                ,outContent.toString().replaceAll("[\\t\\n\\r]", ""));
+        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------" +
+                "连通域顶点数：3" +
+                "Node1: Class1:Func5()" +
+                "Node2: Class1:Func7()" +
+                "Node3: Class1:Func6(java.lang.long)"+
+                "Edge1: Class1:Func5() -- 0.6666 --> Class1:Func7()"+
+                "Edge2: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)"+
+                "-------连通域2-------" +
+                "连通域顶点数：2" +
+                "Node1: Class1:Func1()" +
+                "Node2: Class1:Func2()" +
+                "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()"+
+                        "-------连通域3-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func8()" +
+                        "Node2: Class1:Func9()" +
+                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()"+
+                        "-------连通域4-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func11()" +
+                        "Node2: Class1:Func14()" +
+                        "Edge1: Class1:Func11() -- 1.0000 --> Class1:Func14()".replaceAll("[\\n\\r]", "")
+                ,outContent.toString().replaceAll("[\\n\\r]", ""));
     }
     @Test
     public void OceaniaRunnerTest3(){
@@ -71,49 +71,49 @@ public class OceaniaRunnerTest {
 
         oceaniaRunner.initializeGraph(graphCalculate,"src/test/resources/runner_test_dependency_data.txt");
         oceaniaRunner.closenessFilter(graphCalculate);
-        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------\r\n" +
-                        "连通域顶点数：5\r\n" +
-                        "Node1: Class1:Func1()\r\n" +
-                        "Node2: Class1:Func2()\r\n" +
-                        "Node3: Class1:Func4()\r\n" +
-                        "Node4: Class1:Func3()\r\n" +
-                        "Node5: Class1:Func4(java.lang.long)\r\n" +
-                        "\r\n" +
-                        "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()\r\n" +
-                        "Edge2: Class1:Func2() -- 0.4000 --> Class1:Func4()\r\n" +
-                        "Edge3: Class1:Func1() -- 0.5000 --> Class1:Func4()\r\n" +
-                        "Edge4: Class1:Func2() -- 0.5000 --> Class1:Func3()\r\n" +
-                        "Edge5: Class1:Func2() -- 0.5000 --> Class1:Func4(java.lang.long)\r\n" +
-                        "-------连通域2-------\r\n" +
-                        "连通域顶点数：5\r\n" +
-                        "Node1: Class1:Func10()\r\n" +
-                        "Node2: Class1:Func12()\r\n" +
-                        "Node3: Class1:Func11()\r\n" +
-                        "Node4: Class1:Func13()\r\n" +
-                        "Node5: Class1:Func14()\r\n" +
-                        "\r\n" +
-                        "Edge1: Class1:Func10() -- 0.5000 --> Class1:Func12()\r\n" +
-                        "Edge2: Class1:Func10() -- 0.5000 --> Class1:Func11()\r\n" +
-                        "Edge3: Class1:Func10() -- 0.5000 --> Class1:Func13()\r\n" +
-                        "Edge4: Class1:Func11() -- 1.0000 --> Class1:Func14()\r\n" +
-                        "-------连通域3-------\r\n" +
-                        "连通域顶点数：4\r\n" +
-                        "Node1: Class1:Func5()\r\n" +
-                        "Node2: Class1:Func6()\r\n" +
-                        "Node3: Class1:Func7()\r\n" +
-                        "Node4: Class1:Func6(java.lang.long)\r\n" +
-                        "\r\n" +
-                        "Edge1: Class1:Func5() -- 0.5000 --> Class1:Func6()\r\n" +
-                        "Edge2: Class1:Func5() -- 0.6666 --> Class1:Func7()\r\n" +
-                        "Edge3: Class1:Func7() -- 0.5000 --> Class1:Func6()\r\n" +
-                        "Edge4: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)\r\n" +
-                        "-------连通域4-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func8()\r\n" +
-                        "Node2: Class1:Func9()\r\n" +
-                        "\r\n" +
-                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()\r\n".replaceAll("[\\t\\n\\r]", "")
-                ,outContent.toString().replaceAll("[\\t\\n\\r]", ""));
+        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------" +
+                        "连通域顶点数：5" +
+                        "Node1: Class1:Func1()" +
+                        "Node2: Class1:Func2()" +
+                        "Node3: Class1:Func4()" +
+                        "Node4: Class1:Func3()" +
+                        "Node5: Class1:Func4(java.lang.long)" +
+                        "" +
+                        "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()" +
+                        "Edge2: Class1:Func2() -- 0.4000 --> Class1:Func4()" +
+                        "Edge3: Class1:Func1() -- 0.5000 --> Class1:Func4()" +
+                        "Edge4: Class1:Func2() -- 0.5000 --> Class1:Func3()" +
+                        "Edge5: Class1:Func2() -- 0.5000 --> Class1:Func4(java.lang.long)" +
+                        "-------连通域2-------" +
+                        "连通域顶点数：5" +
+                        "Node1: Class1:Func10()" +
+                        "Node2: Class1:Func12()" +
+                        "Node3: Class1:Func11()" +
+                        "Node4: Class1:Func13()" +
+                        "Node5: Class1:Func14()" +
+                        "" +
+                        "Edge1: Class1:Func10() -- 0.5000 --> Class1:Func12()" +
+                        "Edge2: Class1:Func10() -- 0.5000 --> Class1:Func11()" +
+                        "Edge3: Class1:Func10() -- 0.5000 --> Class1:Func13()" +
+                        "Edge4: Class1:Func11() -- 1.0000 --> Class1:Func14()" +
+                        "-------连通域3-------" +
+                        "连通域顶点数：4" +
+                        "Node1: Class1:Func5()" +
+                        "Node2: Class1:Func6()" +
+                        "Node3: Class1:Func7()" +
+                        "Node4: Class1:Func6(java.lang.long)" +
+                        "" +
+                        "Edge1: Class1:Func5() -- 0.5000 --> Class1:Func6()" +
+                        "Edge2: Class1:Func5() -- 0.6666 --> Class1:Func7()" +
+                        "Edge3: Class1:Func7() -- 0.5000 --> Class1:Func6()" +
+                        "Edge4: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)" +
+                        "-------连通域4-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func8()" +
+                        "Node2: Class1:Func9()" +
+                        "" +
+                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()".replaceAll("[\\n\\r]", "")
+                ,outContent.toString().replaceAll("[\\n\\r]", ""));
     }
 
     @Test
@@ -126,31 +126,31 @@ public class OceaniaRunnerTest {
 
         oceaniaRunner.initializeGraph(graphCalculate,"src/test/resources/runner_test_dependency_data.txt");
         oceaniaRunner.closenessFilter(graphCalculate);
-        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)："+"紧密度阈值必须大于等于0！\r\n"+
-                        "请输入用于过滤的紧密度阈值(非负小数或整数)："+"紧密度阈值必须小于等于1！\r\n"+
-                        "请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------\r\n" +
-                        "连通域顶点数：3\r\n" +
-                        "Node1: Class1:Func5()\r\n" +
-                        "Node2: Class1:Func7()\r\n" +
-                        "Node3: Class1:Func6(java.lang.long)\r\n\r\n"+
-                        "Edge1: Class1:Func5() -- 0.6666 --> Class1:Func7()\r\n"+
-                        "Edge2: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)\r\n"+
-                        "-------连通域2-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func1()\r\n" +
-                        "Node2: Class1:Func2()\r\n\r\n" +
-                        "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()\r\n"+
-                        "-------连通域3-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func8()\r\n" +
-                        "Node2: Class1:Func9()\r\n\r\n" +
-                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()\r\n"+
-                        "-------连通域4-------\r\n" +
-                        "连通域顶点数：2\r\n" +
-                        "Node1: Class1:Func11()\r\n" +
-                        "Node2: Class1:Func14()\r\n\r\n" +
-                        "Edge1: Class1:Func11() -- 1.0000 --> Class1:Func14()\r\n".replaceAll("[\\t\\n\\r]", "")
+        Assert.assertEquals("请输入用于过滤的紧密度阈值(非负小数或整数)："+"紧密度阈值必须大于等于0！"+
+                        "请输入用于过滤的紧密度阈值(非负小数或整数)："+"紧密度阈值必须小于等于1！"+
+                        "请输入用于过滤的紧密度阈值(非负小数或整数)：-------连通域1-------" +
+                        "连通域顶点数：3" +
+                        "Node1: Class1:Func5()" +
+                        "Node2: Class1:Func7()" +
+                        "Node3: Class1:Func6(java.lang.long)"+
+                        "Edge1: Class1:Func5() -- 0.6666 --> Class1:Func7()"+
+                        "Edge2: Class1:Func7() -- 0.6666 --> Class1:Func6(java.lang.long)"+
+                        "-------连通域2-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func1()" +
+                        "Node2: Class1:Func2()" +
+                        "Edge1: Class1:Func1() -- 0.6666 --> Class1:Func2()"+
+                        "-------连通域3-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func8()" +
+                        "Node2: Class1:Func9()" +
+                        "Edge1: Class1:Func8() -- 1.0000 --> Class1:Func9()"+
+                        "-------连通域4-------" +
+                        "连通域顶点数：2" +
+                        "Node1: Class1:Func11()" +
+                        "Node2: Class1:Func14()" +
+                        "Edge1: Class1:Func11() -- 1.0000 --> Class1:Func14()".replaceAll("[\\n\\r]", "")
 
-                ,outContent.toString().replaceAll("[\\t\\n\\r]", ""));
+                ,outContent.toString().replaceAll("[\\n\\r]", ""));
     }
 }
