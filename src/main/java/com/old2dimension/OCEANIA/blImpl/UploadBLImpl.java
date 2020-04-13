6b4e8e5c8f62b092da8da54b7ca885ed7367ad6b
MLN <171250532@smail.nju.edu.cn>
2020-04-13 10:25:46 +0800

    format

--- src/main/java/com/old2dimension/OCEANIA/blImpl/UploadBLImpl.java
+++ src/main/java/com/old2dimension/OCEANIA/blImpl/UploadBLImpl.java
@@ -8,16 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import gr.gousiosg.javacg.stat.JCallGraph;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+
 @Component
 public class UploadBLImpl implements UploadBL {
     @Autowired
     CodeRepository codeRepository;
-    public ResponseVO uploadCode(int userId, MultipartFile[] files) throws IOException  {
-        if (files == null) return  ResponseVO.buildFailure("NULL");
-        if (files.length == 0) return  ResponseVO.buildFailure("EMPTY");
+
+    public ResponseVO uploadCode(int userId, MultipartFile[] files) throws IOException {
+        if (files == null) return ResponseVO.buildFailure("NULL");
+        if (files.length == 0) return ResponseVO.buildFailure("EMPTY");
 
         Code code = new Code();
         code.setName("name");
@@ -29,25 +32,28 @@ public class UploadBLImpl implements UploadBL {
 
             //--------------创建目录和文件-------------
             String fileFullName = multipartFile.getOriginalFilename();
-            if(fileFullName==null){
+            if (fileFullName == null) {
                 return ResponseVO.buildFailure("file error");
             }
-            String dirName = "src/main/resources/analyzeCode/src/"+ (savedCode.getId()+"") + "/"+fileFullName.substring(0,fileFullName.lastIndexOf("/"));
-            String fileName = fileFullName.substring(fileFullName.lastIndexOf("/")+1);
-            fileFullName = dirName +"/"+ fileName;
+            String dirName = "src/main/resources/analyzeCode/src/" + (savedCode.getId() + "") + "/" + fileFullName.substring(0, fileFullName.lastIndexOf("/"));
+            String fileName = fileFullName.substring(fileFullName.lastIndexOf("/") + 1);
+            fileFullName = dirName + "/" + fileName;
             File dir = new File(dirName);
-            if(!dir.exists()){
+            if (!dir.exists()) {
                 boolean isSuccess = dir.mkdirs();
-                if(!isSuccess){
+                if (!isSuccess) {
                     System.out.println(dirName);
-                    return ResponseVO.buildFailure("mkdirs fails");}
+                    return ResponseVO.buildFailure("mkdirs fails");
+                }
             }
 
             File javaFile = new File(fileFullName);
-            if(!javaFile.exists()){
+            if (!javaFile.exists()) {
 
                 boolean isSuccess = javaFile.createNewFile();
-                if(!isSuccess){return ResponseVO.buildFailure("create file fails");}
+                if (!isSuccess) {
+                    return ResponseVO.buildFailure("create file fails");
+                }
             }
             //----------------------------------
             String content = new BufferedReader(new InputStreamReader(multipartFile.getInputStream())).lines().collect(Collectors.joining(System.lineSeparator()));
@@ -55,95 +61,93 @@ public class UploadBLImpl implements UploadBL {
             out.write(content.getBytes());
         }
 
-        return  ResponseVO.buildSuccess(savedCode);
+        return ResponseVO.buildSuccess(savedCode);
     }
 
 
-    public ResponseVO analyzeJar(int codeId) throws IOException{
+    public ResponseVO analyzeJar(int codeId) throws IOException {
 
 
-        File basicDir = new File("src/main/resources/analyzeCode/src/"+codeId);
-        if(!basicDir.exists()){
+        File basicDir = new File("src/main/resources/analyzeCode/src/" + codeId);
+        if (!basicDir.exists()) {
             return ResponseVO.buildFailure("dictionary does not exist,please upload code");
         }
         ArrayList<String> packageStrings = getPackages(basicDir);
 
         Code code = codeRepository.findCodeById(codeId);
-        if(code == null){
+        if (code == null) {
             return ResponseVO.buildFailure("no such code");
         }
         String jarPath = "src/main/resources/jars";
-        if(code.getIs_default() == 1){
+        if (code.getIs_default() == 1) {
             jarPath += "/0.jar";
+        } else {
+            jarPath += "/" + codeId + ".jar";
         }
-        else{
-            jarPath += "/"+codeId+".jar";
-        }
-        String [] args = {jarPath};
+        String[] args = {jarPath};
 
-        File dependencies = new File("src/main/resources/dependencies/"+codeId+".txt");
+        File dependencies = new File("src/main/resources/dependencies/" + codeId + ".txt");
 
-        if(dependencies.exists()){
+        if (dependencies.exists()) {
             boolean isSuccess = dependencies.delete();
         }
-        boolean isSuccess= dependencies.createNewFile();
+        boolean isSuccess = dependencies.createNewFile();
 
         PrintStream psOld = System.out; // 保存原来的输出路径
         System.setOut(new PrintStream(dependencies));// 设置输出重新定向到文件
-        try{
-        JCallGraph.main(args);
-        }
-        catch (Exception e){
+        try {
+            JCallGraph.main(args);
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseVO.buildFailure("Call-Graph error");
         }
         System.setOut(psOld);
-        filterDependencies("src/main/resources/dependencies/"+codeId+".txt",packageStrings);
+        filterDependencies("src/main/resources/dependencies/" + codeId + ".txt", packageStrings);
 
         return ResponseVO.buildSuccess();
     }
 
-    private void filterDependencies(String fileName,ArrayList<String> packageNames) throws IOException{
+    private void filterDependencies(String fileName, ArrayList<String> packageNames) throws IOException {
         File file = new File(fileName);
-        if(!file.exists()){
+        if (!file.exists()) {
             System.out.println("文件不存在");
-            return ;
+            return;
         }
 
-        if(!file.exists()||file.isDirectory())
+        if (!file.exists() || file.isDirectory())
             throw new FileNotFoundException();
-        BufferedReader br=new BufferedReader(new FileReader(file));
-        String temp=null;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String temp = null;
         StringBuffer stringBuffer = new StringBuffer();
-        temp=br.readLine();
+        temp = br.readLine();
 
-        while(temp!=null){
+        while (temp != null) {
 
-            if(temp.substring(0,2).equals("C:")){
-                temp="";
+            if (temp.substring(0, 2).equals("C:")) {
+                temp = "";
             }
             String[] vs = temp.split(" ");
 
-            for(String v : vs){
+            for (String v : vs) {
                 boolean valid = false;
-                for(String packageName : packageNames){
-                    if(v.contains(packageName+".")){
+                for (String packageName : packageNames) {
+                    if (v.contains(packageName + ".")) {
                         valid = true;
                         break;
                     }
                 }
-                if(!valid){
-                    temp="";
+                if (!valid) {
+                    temp = "";
                     break;
                 }
             }
 
-            if(!temp.equals("")){
+            if (!temp.equals("")) {
                 stringBuffer.append(temp);
                 stringBuffer.append("\n");
             }
 
-            temp=br.readLine();
+            temp = br.readLine();
 
         }
 
@@ -155,44 +159,42 @@ public class UploadBLImpl implements UploadBL {
 
     }
 
-    private ArrayList<String> getPackages(File file){
+    private ArrayList<String> getPackages(File file) {
         ArrayList<String> res = new ArrayList<String>();
 
-            File[] files = file.listFiles();
-            if(files == null || files.length==0){
-                return res;
-            }
-            for(File cur : files){
-                if(cur.isDirectory()){
-                    ArrayList<String> temp = getPackages(cur);
-                    for(String str : temp){
-                        res.add(str);
-                    }
+        File[] files = file.listFiles();
+        if (files == null || files.length == 0) {
+            return res;
+        }
+        for (File cur : files) {
+            if (cur.isDirectory()) {
+                ArrayList<String> temp = getPackages(cur);
+                for (String str : temp) {
+                    res.add(str);
                 }
-                else if(cur.isFile()){
-                    String curName = cur.getName();
-                    if(curName.substring(curName.length()-5,curName.length()).equals(".java")){
-                        BufferedReader br = null;
-                        try{
-                            br = new BufferedReader(new FileReader(cur));
-                            String tempStr;
-                            while ((tempStr = br.readLine()) != null) {
-                               if(tempStr.contains("package")){
-                                   String packageString = tempStr.split("package")[1];
-                                   packageString = packageString.substring(0,packageString.length()-1);
-                                   packageString = packageString.trim();
-                                   res.add(packageString);
-                                   break;
-                               }
+            } else if (cur.isFile()) {
+                String curName = cur.getName();
+                if (curName.substring(curName.length() - 5, curName.length()).equals(".java")) {
+                    BufferedReader br = null;
+                    try {
+                        br = new BufferedReader(new FileReader(cur));
+                        String tempStr;
+                        while ((tempStr = br.readLine()) != null) {
+                            if (tempStr.contains("package")) {
+                                String packageString = tempStr.split("package")[1];
+                                packageString = packageString.substring(0, packageString.length() - 1);
+                                packageString = packageString.trim();
+                                res.add(packageString);
+                                break;
                             }
                         }
-                        catch (IOException e){
-                            e.printStackTrace();
-                        }
-
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+
                 }
             }
+        }
         return res;
     }
 
