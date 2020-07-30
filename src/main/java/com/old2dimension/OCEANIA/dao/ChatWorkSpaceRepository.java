849b432f0223345f22e90907a381853f24e6bfe6
171250640 <171250640@smail.nju.edu.cn>
2020-07-30 21:26:14 +0800

    修改配置

--- src/main/java/com/old2dimension/OCEANIA/dao/ChatWorkSpaceRepository.java
+++ src/main/java/com/old2dimension/OCEANIA/dao/ChatWorkSpaceRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ChatWorkSpaceRepository extends JpaRepository<ChatWorkSpace,Integer> {
     ChatWorkSpace findChatWorkSpaceByUserId(int userId);
+
+    List<ChatWorkSpace> findChatWorkSpacesByUserId(int userId);
 }
