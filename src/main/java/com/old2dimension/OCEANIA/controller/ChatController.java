9740ba660f57a2ec962e76310e1f0a9e9b85af10
171250640 <171250640@smail.nju.edu.cn>
2020-07-30 14:36:30 +0800

    添加controller调用

--- src/main/java/com/old2dimension/OCEANIA/controller/ChatController.java
+++ src/main/java/com/old2dimension/OCEANIA/controller/ChatController.java
@@ -1,9 +1,11 @@
 package com.old2dimension.OCEANIA.controller;
 
+import com.old2dimension.OCEANIA.bl.ChatBL;
 import com.old2dimension.OCEANIA.po.ChatMessage;
 import com.old2dimension.OCEANIA.po.ChatWorkSpace;
 import com.old2dimension.OCEANIA.vo.ResponseVO;
 import com.old2dimension.OCEANIA.vo.UserIdAndMessageIdsForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,23 +13,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
+    @Autowired
+    ChatBL chatBL;
     @RequestMapping(value = "/sendMessage",method = RequestMethod.POST)
-    public ResponseVO sendMessage(@RequestBody ChatMessage chatMessage){return null;}
+    public ResponseVO sendMessage(@RequestBody ChatMessage chatMessage){
+       return chatBL.sendMessage(chatMessage);
+    }
 
-    @RequestMapping(value = "/getUnreadMessage/{userId}",method = RequestMethod.GET)
-    public ResponseVO getUnreadMessage(@PathVariable int userId){return null;}
+    @RequestMapping(value = "/getUnreadMessages/{userId}",method = RequestMethod.GET)
+    public ResponseVO getUnreadMessages(@PathVariable int userId){
+        return chatBL.getUnreadMessage(userId);
+    }
 
     @RequestMapping(value = "/saveWorkSpace",method = RequestMethod.POST)
-    public ResponseVO saveWorkSpace(@RequestBody ChatWorkSpace chatWorkSpace){return null;}
+    public ResponseVO saveWorkSpace(@RequestBody ChatWorkSpace chatWorkSpace){return chatBL.saveWorkSpace(chatWorkSpace);}
 
     @RequestMapping(value = "/getChattingRecords",method = RequestMethod.POST)
-    public ResponseVO getChattingRecords(@RequestBody List<Integer> userIds){return null;}
+    public ResponseVO getChattingRecords(@RequestBody List<Integer> userIds){return chatBL.getChattingRecords(userIds);}
 
     @RequestMapping(value = "/getWorkSpace/{userId}",method = RequestMethod.GET)
-    public ResponseVO getWorkSpace(@PathVariable int userId){return null;}
+    public ResponseVO getWorkSpace(@PathVariable int userId){return chatBL.getWorkSpace(userId);}
 
 
     @RequestMapping(value = "/readMessages",method = RequestMethod.POST)
-    public ResponseVO readMessages(UserIdAndMessageIdsForm userIdAndMessageIdsForm){return null;}
+    public ResponseVO readMessages(UserIdAndMessageIdsForm userIdAndMessageIdsForm){return chatBL.readMessages(userIdAndMessageIdsForm);}
 
 }
