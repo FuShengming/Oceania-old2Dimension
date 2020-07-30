a25fdb3564965a80c5f9e44fe239bc928143a2c6
MLN <171250532@smail.nju.edu.cn>
2020-07-30 19:13:49 +0800

    navbar

--- src/main/resources/static/js/notification.js
+++ src/main/resources/static/js/notification.js
@@ -14,19 +14,40 @@ $(function () {
                 if (data.success) {
                     let h = "";
                     data.content.forEach(function (e) {
-                        h += `
+                        let inviter = "";
+                        let group_name = "";
+                        $.ajax({
+                            type: "get",
+                            url: "/user/getById?id=" + e.inviterId,
+                            headers: {"Authorization": $.cookie('token')},
+                            dataType: "json",
+                            contentType: 'application/json',
+                            success: function (data) {
+                                if (data.success) {
+                                    console.log(data.content);
+                                    inviter = data.content[0].name;
+                                    h += `
             <div class="card m-2">
                 <div class="card-body m-0 h5">
-                    <div>${e.groupId} invite you to join in ${e.inviterId}.</div>
+                    <div>${inviter} invite you to join in group ${e.groupId}.</div>
                     <div class="my-2" style="text-align: right">
-                        <button class="btn btn-primary" i-id="${e.id}">Agree</button>
-                        <button class="btn btn-danger" i-id="${e.id}">Reject</button>
+                        <button class="btn btn-primary iv-agree" i-id="${e.id}">Agree</button>
+                        <button class="btn btn-danger iv-refuse" i-id="${e.id}">Refuse</button>
                     </div>
                 </div>
             </div>
 `;
+                                    $("#invitation-container").html(h);
+                                } else {
+                                    console.log(data.message);
+                                }
+                            },
+                            error: function (err) {
+                                console.log(err);
+                            }
+                        });
                     });
-                    $("#invitation-container").html(h);
+
                 } else {
                     console.log(data.message);
                 }
